ec92402feaff69e1554ebecb094195cd695981ea
Borys Ushakov <inlvert@gmail.com>
2024-01-09 11:51:34 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import HomePage from "./pages/Home";
+
+function App() {
+  return (
+    <Router>
+      <div style={{ color: "red", display: "flex", flexDirection: "row", margin: '20px', }}>
+        <nav>
+          <Link to="/about">About</Link>
+          <Link to="/">Home</Link>
+          <Link to="/users">Users</Link>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+export default App;
